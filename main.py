03357392f85a8770bd0383392e0addf5499ed2e8
gundambox <7722420+gundambox@users.noreply.github.com>
2024-10-26 02:53:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,152 +3,183 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime
+import datetime
+import os
 
-# # Step 1: Download Historical Data for ^TWII
-# # Retrieve historical data for ^TWII starting from 2004/01/01 to the present
-# # Discard non-trading days (twiiHistory)
-# twii = yf.Ticker("^TWII")
-# twiiHistory = twii.history(period="max")
-# twiiHistory = twiiHistory.dropna()
-# twiiHistory.to_csv('twiiHistory.csv')
-# # Step 2: Download Historical Data for 00631L.TW
-# # Retrieve historical data for 00631L.TW starting from 2014/12/01 to the present
-# # Discard non-trading days (etf00631LHistory)
-# etf00631L = yf.Ticker("00631L.TW")
-# etf00631LHistory = etf00631L.history(start="2014-12-01")
-# etf00631LHistory = etf00631LHistory.dropna()
-# etf00631LHistory.to_csv('etf00631LHistory.csv')
+DOLLOR_TO_TWD = 32.07
+ticket_list = ["^TWII", "00631L.TW", "SPY", "SSO"]
 
-# # Read from CSV
-# twiiHistory = pd.read_csv('twiiHistory.csv', index_col='Date', parse_dates=True)
-# etf00631LHistory = pd.read_csv('etf00631LHistory.csv', index_col='Date', parse_dates=True)
+def download_data(path):
+    # Download the data from Yahoo Finance
+    history = yf.download(ticket_list, period='max', ignore_tz=True)
+    history = history['Close']
+    history.to_csv(path)
+    return history
 
-# # Step 3: Simulate 00631L.TW's Returns Using ^TWII Data
-# # Use the ^TWII percetage change data from 2004/01/01 to 2014/11/30 to simulate the returns of 00631L.TW
-# #   Calculate the average and standard deviation of 00631L.TW's percetage change (etf00631LMean, etf00631LStd)
-# #   Calculate the average and standard deviation of ^TWII's percetage change (twiiMean, twiiStd)
-# #   Simulate the percetage change of 00631L.TW by multiplying ^TWII's percetage change by 2 and adding random noise based on 00631L.TW's historical returns
-# #   Combine the historical percetage change of 00631L.TW with the simulated returns to form the complete data set (etf00631LFinal)
-# #   Change column name "Close" to "Percentage" and save the simulated data set to a CSV file
-# etf00631LPctChange = etf00631LHistory['Close'].pct_change()
-# etf00631LMean = etf00631LPctChange.mean()
-# etf00631LStd = etf00631LPctChange.std()
-# # print('etf00631LMean:', etf00631LMean)
-# # print('etf00631LStd:', etf00631LStd)
-# twiiPctChange = twiiHistory['Close'].pct_change()
-# twiiMean = twiiPctChange.mean()
-# twiiStd = twiiPctChange.std()
-# # print('twiiMean:', twiiMean)
-# # print('twiiStd:', twiiStd)
+def load_data(path):
+    if os.path.exists(path):
+        history = pd.read_csv(path)
+        # Convert column name 'Ticker' to 'Date' and set it as index
+        last_trading_day = pd.to_datetime(history['Date']).max().to_pydatetime().date()
+        # if last trading day is not today, download the latest data
+        if last_trading_day < (datetime.datetime.today() + datetime.timedelta(days=-1)).date():
+            print("Download the latest data")
+            history = download_data(path)
+    else:
+        history = download_data(path)
 
-# np.random.seed(0)
-# simulateLength = len(twiiPctChange) - len(etf00631LPctChange)
-# simulated_etf00631LPctChange = twiiPctChange[:simulateLength] * 2
+    return history
 
-# # concat simulated_etf00631LPctChange and etf00631LPctChange and name the columen as 'Percentage'
-# etf00631LFinal = pd.concat([simulated_etf00631LPctChange, etf00631LPctChange])
-# etf00631LFinal = pd.DataFrame(etf00631LFinal, columns=['Close'])
-# etf00631LFinal = etf00631LFinal.rename(columns={'Close': 'Percentage'})
-# etf00631LFinal = etf00631LFinal.dropna()
+def extend_leverage_data(origin_data, leverage_data):
+    origin_pct_change_data = origin_data['Price'].pct_change(fill_method=None)
+    leverage_pct_change_data = leverage_data['Price'].pct_change(fill_method=None)
+    np.random.seed(0)
 
-# twiiFinal = twiiPctChange.copy()
-# twiiFinal = pd.DataFrame(twiiFinal, index=twiiHistory.index, columns=['Close'])
-# twiiFinal = twiiFinal.rename(columns={'Close': 'Percentage'})
-# twiiFinal = twiiFinal.dropna()
+    # Find the first valid index of the origin_pct_change_data
+    origin_pct_change_data_idx = pd.Series(origin_pct_change_data).first_valid_index()
+    # Find the first valid index of the leverage_pct_change_data
+    leverage_pct_change_data_idx = pd.Series(leverage_pct_change_data).first_valid_index()
 
-# # # # Step 4: Find the Intersection of Both Data Sets
-# # # # Retain only the trading days present in both data sets
-# # # # This ensures that we have a common set of dates for analysis
-# # # # Save the intersected data sets to CSV files
-# common_dates = twiiFinal.index.intersection(etf00631LFinal.index)
-# twiiFinal = twiiFinal.loc[common_dates]
-# etf00631LFinal = etf00631LFinal.loc[common_dates]
+    # pick the range from origin_pct_change_data_idx to leverage_pct_change_data_idx and multiply 2 and reassign back to leverage_pct_change_data
+    leverage_final = pd.concat([origin_pct_change_data[:origin_pct_change_data_idx],
+                                origin_pct_change_data[origin_pct_change_data_idx:leverage_pct_change_data_idx] * 2,
+                                leverage_pct_change_data[leverage_pct_change_data_idx:]])
+    leverage_final = pd.DataFrame({'Date': origin_data['Date'], 'Percentage': leverage_final})
+    return leverage_final
 
-# twiiFinal.to_csv('twiiFinal.csv')
-# etf00631LFinal.to_csv('etf00631LFinal.csv')
+def prompt_user_input_date():
+    # start_date = input("Enter the start date (YYYY-MM-DD): ")
+    # # if start date is not provided, return default start date
+    # if not start_date:
+    return "2004-01-01"
+    # else:
+    #     return start_date
 
-twiiFinal = pd.read_csv('twiiFinal.csv', index_col='Date', parse_dates=True)
-etf00631LFinal = pd.read_csv('etf00631LFinal.csv', index_col='Date', parse_dates=True)
+def filter_data_by_start_date(data, start_date):
+    return data[data['Date']>=start_date]
 
-# Change index format to python date
-twiiFinal.index = pd.to_datetime(twiiFinal.index)
-etf00631LFinal.index = pd.to_datetime(etf00631LFinal.index)
+history = load_data("ticket.csv")
+tradingDate = pd.to_datetime(history['Date'])
+# Convert the index to datetime
+twiiHistory =  pd.DataFrame({'Date':tradingDate, 'Price': history['^TWII']})
+etf00631LHistory = pd.DataFrame({'Date':tradingDate, 'Price': history['00631L.TW']})
+etfSpyHistory = pd.DataFrame({'Date':tradingDate, 'Price': history['SPY']})
+etfSsoHistory = pd.DataFrame({'Date':tradingDate, 'Price': history['SSO']})
 
-# Ask for user input to get the start date
-start_date = input("Enter the start date (YYYY-MM-DD): ")
-# Filter the data based on the start date, if the start date is not in the data set, filter out the dates before the start date
-twiiFinal = twiiFinal.loc[(twiiFinal.index >= start_date)]
-etf00631LFinal = etf00631LFinal.loc[(etf00631LFinal.index >= start_date)]
+# Calculate the percentage change of the stock
+etf00631LFinal = extend_leverage_data(twiiHistory, etf00631LHistory).fillna(0)
+etfSsoFinal = extend_leverage_data(etfSpyHistory, etfSsoHistory).fillna(0)
+twiiFinal = pd.DataFrame({'Date':tradingDate, 'Percentage': twiiHistory['Price'].pct_change(fill_method=None)}).fillna(0)
+etfSpyFinal = pd.DataFrame({'Date':tradingDate, 'Percentage': etfSpyHistory['Price'].pct_change(fill_method=None)}).fillna(0)
 
-# Step 5: Investment Simulation Starting with 1,000,000 TWD
-# Assume an initial investment of 1,000,000 TWD on the first trading day after 2004/01/01
-# Simulate asset changes using the following strategies:
-#   Strategy 1: Reallocate all assets to ^TWII on the first trading day of each year
-#   Strategy 2: Reallocate all assets to 00631L.TW on the first trading day of each year
-#   Strategy 3: Allocate 40% of assets to 00631L.TW and hold the remainder in cash on the first trading day of each year
-#   Strategy 4: Allocate 50% of assets to 00631L.TW and hold the remainder in cash on the first trading day of each year
-#   Strategy 5: Allocate 60% of assets to 00631L.TW and hold the remainder in cash on the first trading day of each year
+start_date = prompt_user_input_date()
 
-initial_investment = 1000000
+# Filter the data by start date
+tradingDate = tradingDate[tradingDate >= start_date]
+
+twiiFinal = filter_data_by_start_date(twiiFinal, start_date)
+etf00631LFinal = filter_data_by_start_date(etf00631LFinal, start_date)
+etfSpyFinal = filter_data_by_start_date(etfSpyFinal, start_date)
+etfSsoFinal = filter_data_by_start_date(etfSsoFinal, start_date)
+
+etf00631LFinal.to_csv("00631L.TW.csv")
+etfSsoFinal.to_csv("SSO.csv")
+twiiFinal.to_csv("^TWII.csv")
+etfSpyFinal.to_csv("SPY.csv")
+
+initial_investment = 1200000
 strategies = {
-    "Strategy 1": {"twii": 1.0, "etf": 0.0, "cash": 0.0},
-    "Strategy 2": {"twii": 0.0, "etf": 1.0, "cash": 0.0},
-    "Strategy 3": {"twii": 0.0, "etf": 0.4, "cash": 0.6},
-    "Strategy 4": {"twii": 0.0, "etf": 0.5, "cash": 0.5},
-    "Strategy 5": {"twii": 0.0, "etf": 0.6, "cash": 0.4},
+    "Strategy 1": {"cash-TWD": 0.0, "cash-USD": 0.0, "stock-twii": 1.0, "stock-00631L": 0.0, "stock-SPY": 0.0, "stock-SSO": 0.0},
+    "Strategy 2": {"cash-TWD": 0.0, "cash-USD": 0.0, "stock-twii": 0.0, "stock-00631L": 1.0, "stock-SPY": 0.0, "stock-SSO": 0.0},
+    "Strategy 3": {"cash-TWD": 0.0, "cash-USD": 0.0, "stock-twii": 0.0, "stock-00631L": 0.0, "stock-SPY": 1.0, "stock-SSO": 0.0},
+    "Strategy 4": {"cash-TWD": 0.0, "cash-USD": 0.0, "stock-twii": 0.0, "stock-00631L": 0.0, "stock-SPY": 0.0, "stock-SSO": 1.0},
+    "Strategy 5": {"cash-TWD": 0.4, "cash-USD": 0.1, "stock-twii": 0.0, "stock-00631L": 0.4, "stock-SPY": 0.0, "stock-SSO": 0.1}, # 100%
+    "Strategy 6": {"cash-TWD": 0.2, "cash-USD": 0.1, "stock-twii": 0.0, "stock-00631L": 0.5, "stock-SPY": 0.2, "stock-SSO": 0.0}, # 120%
+    "Strategy 7": {"cash-TWD": 0.3, "cash-USD": 0.2, "stock-twii": 0.0, "stock-00631L": 0.3, "stock-SPY": 0.0, "stock-SSO": 0.2}, # 100%
+    "Strategy 8": {"cash-TWD": 0.25, "cash-USD": 0.25, "stock-twii": 0, "stock-00631L": 0.25, "stock-SPY": 0.0, "stock-SSO": 0.25}, # 100%
+    "Strategy 9": {"cash-TWD": 0.5, "cash-USD": 0.0, "stock-twii": 0.0, "stock-00631L": 0.5, "stock-SPY": 0.0, "stock-SSO": 0.0}, # 100%
 }
 
 # Initialize the asset changes for each strategy
 asset_changes = { strategy: {asset: [int(initial_investment*ratio)] for asset, ratio in allocation.items()} for strategy, allocation in strategies.items() }
 
-# Get common dates
-common_dates = twiiFinal.index.intersection(etf00631LFinal.index)
-
 # Allocate assets based on each strategy first trading day of each year
-last_year = twiiFinal.index[0].year
-for date in common_dates:
+last_year = datetime.datetime.today().year
+
+for date in tradingDate:
     # Check if the year has changed
-    if date.year != last_year:
+    if pd.to_datetime(date).year != last_year:
+        # Rebalance the portfolio
+        print(f"First trading day of the year {pd.to_datetime(date).year}, rebalance the portfolio")
         for strategy, allocation in strategies.items():
             # reallocate assets based on the strategy
-            total_value = asset_changes[strategy]["twii"][-1] + asset_changes[strategy]["etf"][-1] + asset_changes[strategy]["cash"][-1]
-            twii_allocation = total_value * allocation["twii"]
-            etf_allocation = total_value * allocation["etf"]
-            cash_allocation = total_value * allocation["cash"]
-            # Update asset changes, because we are using currency, we round to integer
-            asset_changes[strategy]["twii"].append(int(twii_allocation * (1 + twii_change)))
-            asset_changes[strategy]["etf"].append(int(etf_allocation * (1 + etf_change)))
-            asset_changes[strategy]["cash"].append(int(cash_allocation))
+            asset_cashTWD = asset_changes[strategy]["cash-TWD"][-1]
+            asset_cashUSD = asset_changes[strategy]["cash-USD"][-1]
+            asset_stockWwii = asset_changes[strategy]["stock-twii"][-1]
+            asset_stock00631L = asset_changes[strategy]["stock-00631L"][-1]
+            asset_stockSpy = asset_changes[strategy]["stock-SPY"][-1]
+            asset_stockSso = asset_changes[strategy]["stock-SSO"][-1]
+            total_value = asset_cashTWD + asset_cashUSD + asset_stockWwii + asset_stock00631L + asset_stockSpy + asset_stockSso
 
-        last_year = date.year
+            cashTWD_allocation = total_value * allocation["cash-TWD"]
+            cashUSD_allocation = total_value * allocation["cash-USD"]
+            twii_allocation = total_value * allocation["stock-twii"]
+            etf_allocation = total_value * allocation["stock-00631L"]
+            spy_allocation = total_value * allocation["stock-SPY"]
+            sso_allocation = total_value * allocation["stock-SSO"]
 
-    else:
-        for strategy, allocation in strategies.items():
-            twii_change = twiiFinal.loc[date, "Percentage"]
-            etf_change = etf00631LFinal.loc[date, "Percentage"]
+            # Update last record asset changes, because we are using currency, we round to integer
+            asset_changes[strategy]["cash-TWD"][-1] = int(cashTWD_allocation)
+            asset_changes[strategy]["cash-USD"][-1] = int(cashUSD_allocation)
+            asset_changes[strategy]["stock-twii"][-1] = int(twii_allocation)
+            asset_changes[strategy]["stock-00631L"][-1] = int(etf_allocation)
+            asset_changes[strategy]["stock-SPY"][-1] = int(spy_allocation)
+            asset_changes[strategy]["stock-SSO"][-1] = int(sso_allocation)
+        last_year = pd.to_datetime(date).year
 
-            asset_changes[strategy]["twii"].append(int(asset_changes[strategy]["twii"][-1] * (1 + twii_change)))
-            asset_changes[strategy]["etf"].append(int(asset_changes[strategy]["etf"][-1] * (1 + etf_change)))
-            asset_changes[strategy]["cash"].append(int(asset_changes[strategy]["cash"][-1]))
+    for strategy, allocation in strategies.items():
+        # Get the percentage change of the stock according to the date, if this date does not have trading data, assume the stock price does not change
+        twii_change = twiiFinal.loc[twiiFinal['Date'] == date, 'Percentage']
+        asset_changes[strategy]["stock-twii"].append(int(asset_changes[strategy]["stock-twii"][-1] * (1 + twii_change)))
 
-# Save changes to CSV
-for strategy, changes in asset_changes.items():
-    # Ensure the lengths match
-    min_length = min(len(common_dates), len(changes["twii"]))
-    df = pd.DataFrame({asset: values[:min_length] for asset, values in changes.items()}, index=common_dates[:min_length])
-    df.to_csv(f'{strategy}.csv')
+        etf00631L_change = etf00631LFinal.loc[etf00631LFinal['Date'] == date, 'Percentage']
+        asset_changes[strategy]["stock-00631L"].append(int(asset_changes[strategy]["stock-00631L"][-1] * (1 + etf00631L_change)))
 
+        etfSpy_change = etfSpyFinal.loc[etfSpyFinal['Date'] == date, 'Percentage']
+        asset_changes[strategy]["stock-SPY"].append(int(asset_changes[strategy]["stock-SPY"][-1] * (1 + etfSpy_change)))
 
-# Step 6: Calculate Metrics
+        etfSso_change = etfSsoFinal.loc[etfSsoFinal['Date'] == date, 'Percentage']
+        asset_changes[strategy]["stock-SSO"].append(int(asset_changes[strategy]["stock-SSO"][-1] * (1 + etfSso_change)))
+
+        asset_changes[strategy]["cash-TWD"].append(int(asset_changes[strategy]["cash-TWD"][-1]))
+        asset_changes[strategy]["cash-USD"].append(int(asset_changes[strategy]["cash-USD"][-1]))
+
+# Save the asset changes to csv
+min_length = len(tradingDate)
+for strategy, allocation in asset_changes.items():
+    min_length = min(len(tradingDate), len(allocation['cash-TWD']), len(allocation['cash-USD']), len(allocation['stock-twii']), len(allocation['stock-00631L']), len(allocation['stock-SPY']), len(allocation['stock-SSO']))
+    tradingDate = tradingDate[:min_length]
+    df = pd.DataFrame({'Date': tradingDate,
+                        'cash-TWD': allocation["cash-TWD"][0:min_length],
+                        'cash-USD': allocation["cash-USD"][0:min_length],
+                        'stock-twii': allocation["stock-twii"][0:min_length],
+                        'stock-00631L': allocation["stock-00631L"][0:min_length],
+                        'stock-SPY': allocation["stock-SPY"][0:min_length],
+                        'stock-SSO': allocation["stock-SSO"][0:min_length]})
+    df.to_csv(f"{strategy}.csv")
+
 # For each strategy, calculate the annualized return, standard deviation, Sharpe ratio, maximum drawdown, maximum consecutive gain days, and maximum consecutive loss days
 # Print the metrics for each strategy
 for strategy, allocation in strategies.items():
     print(f"Strategy: {strategy}")
-    twii_values = asset_changes[strategy]["twii"]
-    etf_values = asset_changes[strategy]["etf"]
-    cash_values = asset_changes[strategy]["cash"]
-    total_values = [twii + etf + cash for twii, etf, cash in zip(twii_values, etf_values, cash_values)]
+    stockTwii_values = asset_changes[strategy]["stock-twii"]
+    stock00631L_values = asset_changes[strategy]["stock-00631L"]
+    stockSpy_values = asset_changes[strategy]["stock-SPY"]
+    stockSso_values = asset_changes[strategy]["stock-SSO"]
+    cashTWS_values = asset_changes[strategy]["cash-TWD"]
+    cashUSD_values = asset_changes[strategy]["cash-USD"]
+
+    total_values = [twii + etf + spy + sso + cashTWD + cashUSD for twii, etf, spy, sso, cashTWD, cashUSD in zip(stockTwii_values, stock00631L_values, stockSpy_values, stockSso_values, cashTWS_values, cashUSD_values)]
     total_returns = pd.Series(total_values)
     cumulative_return = (total_returns.iloc[-1] - total_returns.iloc[0]) / total_returns.iloc[0]
     annualized_return = (total_returns.iloc[-1] / total_returns.iloc[0]) ** (252 / len(total_returns)) - 1
@@ -163,6 +194,15 @@ for strategy, allocation in strategies.items():
     print(f"Max Drawdown: {max_drawdown:.2%}")
     print("="*40)
 
+    # Save report to file
+    with open(f"{strategy}_report.txt", "w") as f:
+        f.write(f"Strategy: {strategy}\n")
+        f.write(f"Cumulative Return: {cumulative_return:.2%}\n")
+        f.write(f"Annualized Return: {annualized_return:.2%}\n")
+        f.write(f"Annualized Standard Deviation: {annualized_std:.2%}\n")
+        f.write(f"Sharpe Ratio: {sharpe_ratio:.2f}\n")
+        f.write(f"Max Drawdown: {max_drawdown:.2%}\n")
+
 # Step 7: Plot Asset Changes
 # Create a visualization of asset changes over time for each strategy
 # Save the plot to a file
@@ -170,11 +210,15 @@ for strategy, allocation in strategies.items():
 plt.figure(figsize=(14, 7))
 
 for strategy, allocation in strategies.items():
-    twii_values = asset_changes[strategy]["twii"]
-    etf_values = asset_changes[strategy]["etf"]
-    cash_values = asset_changes[strategy]["cash"]
-    total_values = [twii + etf + cash for twii, etf, cash in zip(twii_values, etf_values, cash_values)]
-    plt.plot(common_dates, total_values[:len(common_dates)], label=strategy)
+    stockTwii_values = asset_changes[strategy]["stock-twii"][:min_length]
+    stock00631L_values = asset_changes[strategy]["stock-00631L"][:min_length]
+    stockSpy_values = asset_changes[strategy]["stock-SPY"][:min_length]
+    stockSso_values = asset_changes[strategy]["stock-SSO"][:min_length]
+    cashTWS_values = asset_changes[strategy]["cash-TWD"][:min_length]
+    cashUSD_values = asset_changes[strategy]["cash-USD"][:min_length]
+
+    total_values = [twii + etf + spy + sso + cashTWD + cashUSD for twii, etf, spy, sso, cashTWD, cashUSD in zip(stockTwii_values, stock00631L_values, stockSpy_values, stockSso_values, cashTWS_values, cashUSD_values)]
+    plt.plot(tradingDate, total_values, label=strategy)
 
 plt.title('Asset Changes Over Time for Each Strategy')
 plt.xlabel('Date')
